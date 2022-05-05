3487c4a8138fedd03720613ac8422ab20d4fa371
Disco-Science <86618381+Disco-Science@users.noreply.github.com>
2022-05-04 20:36:09 -0700

    Updated to include short summary

--- readSQL_searchBatchID.R
+++ readSQL_searchBatchID.R
@@ -1,5 +1,6 @@
+#Author Nathan Choi; this script is written to pull matching rows (by BATCH.NO) from a MySQL (Oracle) datatable and return that as an xlsx file
+
 library(tidyverse) #Pipes and table functions
-library(xlsx) #Not sure?
 library(openxlsx) #Write out Excel files
 library(readxl) #Read in Excel files
 library(lubridate)#Change dates to date.objects
@@ -7,32 +8,33 @@ library(RODBC) #SQL connection
 library(hablar)
 
 #Create ODBC connection to Oracle DB
-# conn <- RODBC::odbcConnect(dsn = "RTPHPP1_64Bit", 
-#                            uid = "NATHANC", 
-#                            pwd = "Tracker2022!@#Tracker", 
-#                            believeNRows = FALSE) 
+
+conn <- RODBC::odbcConnect(dsn = "DSN", 
+                            uid = "USER", 
+                            pwd = "PW", 
+                            believeNRows = FALSE) 
 
 #SQL query as a string
-# grabTableNames = paste("SELECT table_name from all_tables order by table_name")
-# grabVWAutomation = paste("select * from VWAutomationData;")
-# grabSDO_XML_SCHEMAS = paste("select * from SDO_XML_SCHEMAS;")
+grabTableNames = paste("SELECT table_name from all_tables order by table_name")
+grabVWAutomation = paste("select * from VWAutomationData;")
+grabSDO_XML_SCHEMAS = paste("select * from SDO_XML_SCHEMAS;")
 
 
-# #Run send query to database
-#VWAutomation <- RODBC::sqlQuery(conn, grabVWAutomation) 
-# SDO_XML_SCHEMAS <- RODBC::sqlQuery(conn, grabSDO_XML_SCHEMAS)
-# Table_Names <- RODBC::sqlQuery(conn, grabTableNames)
+#Run send query to database
+VWAutomation <- RODBC::sqlQuery(conn, grabVWAutomation) 
+SDO_XML_SCHEMAS <- RODBC::sqlQuery(conn, grabSDO_XML_SCHEMAS)
+Table_Names <- RODBC::sqlQuery(conn, grabTableNames)
 
 #Close connection
-# close(conn)
+close(conn)
 
-# #Modify table
-# VWAutomation_edit = VWAutomation %>% mutate(
-#     QCMODELSYSTEM = as.character(QCMODELSYSTEM),
-#     SAMPLETYPE = as.character(SAMPLETYPE),
-#     BATCHCOMPLETEDDATE = ymd(BATCHCOMPLETEDDATE)
-#     ) %>% 
-#     filter(QCMODELSYSTEM != "", CORETEAM == c("Legacy", "LEGO", "New Content", "New Dye"))
+#Modify table data types
+VWAutomation_edit = VWAutomation %>% mutate(
+     QCMODELSYSTEM = as.character(QCMODELSYSTEM),
+     SAMPLETYPE = as.character(SAMPLETYPE),
+     BATCHCOMPLETEDDATE = ymd(BATCHCOMPLETEDDATE)
+     ) %>% 
+     filter(QCMODELSYSTEM != "", CORETEAM == c("Legacy", "LEGO", "New Content", "New Dye"))
 
 
 # #Grab Andrew's readout
@@ -67,18 +69,11 @@ TSL_summary_table = FERT %>%
               total.FERTs = length(unique(`FERT`))
     )
 
-# #Create file name
-# fileName = paste("C:/Users/10351517/Documents/Stability/getClones/SQL_View_VWAutomation", Sys.Date(), ".xlsx", sep="")
-# 
-# #Export selectedDF 
-# openxlsx::write.xlsx(VWAutomation, file = fileName)
+#Perform pull of rows from SQL table where batch numbers match
 
-
-
-# #Perform pull of rows from SQL table where batch numbers match
-# returnDB = VWAutomation[VWAutomation$BATCHNO %in% listOfBatchIDs,]
-# 
-# returnFileName = paste("C:/Users/10351517/Documents/Stability/getClones/VWAutomationa_BatchID_Matches", Sys.Date(), ".xlsx", sep="")
-# 
-# openxlsx::write.xlsx(returnDB, file = returnFileName)
+returnDB = VWAutomation[VWAutomation_edit$BATCHNO %in% FERT$BatchIDrequested,]
+ 
+returnFileName = paste("C:/Users/10351517/Documents/Stability/getClones/VWAutomationa_BatchID_Matches", Sys.Date(), ".xlsx", sep="")
+ 
+openxlsx::write.xlsx(returnDB, file = returnFileName)
 
